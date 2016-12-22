5e953b1d673384e991924b4146259e0ba2ca0aed
Igor Dagayev <igor.dagayev@bluepointstudios.com>
2016-12-22 16:09:39 -0500

    helpers

---
+++ javascripts/helpers.js
@@ -0,0 +1,15 @@
+function getDateString (date) {
+  var dateString = date.getMonth() + '/' + date.getDate() + '/';
+  var shortYear = date.getFullYear() - 2000;
+
+  dateString += shortYear + ' ';
+
+  var time = date.getHours();
+  if(time < 12){
+    dateString += time + ' am';
+  }else {
+    dateString += (time - 12) + ' pm';
+  }
+
+  return dateString;
+}
